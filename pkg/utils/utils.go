de94958367eb08280e281d5dc8e9c215f2b36393
Manpreet Kukreja <manpreet1130@gmail.com>
2022-01-20 11:19:37 +0530

    Update utils.go

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,3 +1,7 @@
+// utils.go is responsible for parsing the body sent in the form 
+// of JSON in the HTTP Request and converting it into a structure which
+// Golang can understand
+
 package utils
 
 import (
@@ -7,6 +11,8 @@ import (
 	"net/http"
 )
 
+// ParseBody takes in a Golang readable struct and an io.Reader and 
+// unmarshals the JSON data into the struct
 func ParseBody(x interface{}, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
