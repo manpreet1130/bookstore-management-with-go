63ac934cce59f20975514b7cdc684c7ebf716beb
Manpreet Kukreja <manpreet1130@gmail.com>
2022-01-20 11:30:54 +0530

    Update main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -9,9 +9,14 @@ import (
 )
 
 func main() {
+	// Creating a new router using the gorilla/mux package
 	router := mux.NewRouter()
+	
+	// Calling CreateRoutes function to instantiate all required routes
 	routes.CreateRoutes(router)
 	http.Handle("/", router)
+	
+	// Creating server instance and listening on port 8080
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
